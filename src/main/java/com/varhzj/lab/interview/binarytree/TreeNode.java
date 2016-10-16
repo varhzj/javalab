c4b371b801cc0eeaad5750ab29948542fbccadad
zhijianhuang <zhijianhuang210258@sohu-inc.com>
2016-10-16 13:29:51 +0800

    fix

---
+++ src/main/java/com/varhzj/lab/interview/binarytree/TreeNode.java
@@ -0,0 +1,16 @@
+package com.varhzj.lab.interview.binarytree;
+
+/**
+ * Created by huangzhijian on 16-8-13.
+ */
+public class TreeNode {
+
+    public int value;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int value) {
+        this.value = value;
+    }
+
+}
---
+++ src/main/java/com/varhzj/lab/interview/linkedlist/DoubleListNode.java
@@ -0,0 +1,16 @@
+package com.varhzj.lab.interview.linkedlist;
+
+/**
+ * Created by huangzhijian on 16-8-13.
+ */
+public class DoubleListNode {
+
+    public int value;
+    public DoubleListNode pre;
+    public DoubleListNode next;
+
+    public DoubleListNode(int value) {
+        this.value = value;
+    }
+
+}
