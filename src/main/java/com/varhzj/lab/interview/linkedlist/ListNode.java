09e5075fa580321cb38d4e89479adf68fa1002ee
zhijianhuang <zhijianhuang210258@sohu-inc.com>
2016-12-14 17:40:15 +0800

    new

--- src/main/java/com/varhzj/lab/interview/linkedlist/ListNode.java
+++ src/main/java/com/varhzj/lab/interview/linkedlist/ListNode.java
@@ -5,11 +5,11 @@ package com.varhzj.lab.interview.linkedlist;
  */
 public class ListNode {
 
-    public int value;
+    public int val;
     public ListNode next;
 
-    public ListNode(int value) {
-        this.value = value;
+    public ListNode(int val) {
+        this.val = val;
     }
 
 }
