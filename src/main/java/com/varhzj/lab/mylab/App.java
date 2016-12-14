7c87a3040da27b249997dc84a751334b49dfb498
zhijianhuang <zhijianhuang210258@sohu-inc.com>
2016-12-14 20:39:42 +0800

    fix

--- src/main/java/com/varhzj/lab/mylab/App.java
+++ src/main/java/com/varhzj/lab/mylab/App.java
@@ -6,8 +6,26 @@ package com.varhzj.lab.mylab;
  */
 public class App
 {
+
+    static {
+        printTest();
+    }
+
+    private static final Object obj = new Object();
+
+    public static void printTest() {
+        System.out.println("A test before main");
+    }
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Thread thread1 = new Thread(() -> System.out.println("thread1"));
+        thread1.start();
+//        thread1.start(); // java.lang.IllegalThreadStateException
+        try {
+            obj.wait(); // java.lang.IllegalMonitorStateException
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
